732c5a73db57015831454b73e2c6ba1db2f9c665
bzaire <bonniezaire@gmail.com>
2019-09-08 22:28:22 +0200

    proper way to generate files

--- zdi/create_lsdfile.py
+++ zdi/create_lsdfile.py
@@ -4,12 +4,14 @@ import glob
 from math import modf 
 from .rspec import rstokes
 from .norm import norm, norm_search
+import os
 
-# constants
-lamb_0 = 625. # central lambda in nm
-c = 299792. # speed of light in km/s
-period =  0.521183398 #days
-To = 2440610.06406
+# CONSTANTS
+lamb_0 = 625.          # central lambda in nm
+c = 299792.            # speed of light in km/s
+period =  0.5211833875 # from Vaccaro 2015 
+To = 2445821.898291
+
 
 def wmatrix(file, matrix):
     ms = int(matrix.shape[0]/8)
@@ -17,25 +19,11 @@ def wmatrix(file, matrix):
     for i in range(ms):
         line = tuple(matrix[8*i:8*(i+1)])
         file.write("{:>12.5e} {: >12.5e} {: >12.5e} {: >12.5e} {: >12.5e} {: >12.5e} {: >12.5e} {: >12.5e}\n".format(*line))
-#        file.write('   %1.5e     %1.5e     %1.5e     %1.5e     %1.5e     %1.5e     %1.5e     %1.5e\n' % line)
     if rms!= 0:
         for j in range(rms):
             file.write("{:>12.5e} ".format(matrix[-rms+j]))
         file.write('\n')
     file.write('\n')
-
-#def wmatrix(file, temp_matrix):
-#    matrix = temp_matrix
-#    ms = int(matrix.shape[0]/8)
-#    rms = matrix.shape[0]%8
-#    for i in range(ms):
-#        line = tuple(matrix[8*i:8*(i+1)])
-#        file.write('   %1.5e     %1.5e     %1.5e     %1.5e     %1.5e     %1.5e     %1.5e     %1.5e\n' % line)
-#    if rms!= 0:
-#        for j in range(rms):
-#            file.write('   %.5e  ' % matrix[-rms+j])
-#        file.write('\n')
-#    file.write('\n')
 
 def wfile(wi, phase, vr, snI, I):
     lmin = lamb_0*(vr.min()/c + 1)
@@ -48,226 +36,96 @@ def wfile(wi, phase, vr, snI, I):
     wi.write('\n')
     wmatrix(wi, I)
 
-def wfile_V(wi, phase, vr, snI, I, snV, V):
+def wfile_V(wi, phase, vr, snV, V):
     lmin = lamb_0*(vr.min()/c + 1)
     lmax = lamb_0*(vr.max()/c + 1)
     ncont = 1
     dlamb = lamb_0/65000.
     count = 0.
-    wi.write('Mean    %1.9e  0   1 0 0 0\n' % lamb_0)
-    wi.write('%d  %.6f  %.6f    %1.9e    %1.9e  %1.6e  %1.6e  %1.6e  %.6f\n'% (len(I), phase, phase, lmin, lmax, snI, ncont, dlamb, count))
-    wi.write('\n')
-    wmatrix(wi, I)
     wi.write('Mean    %1.9e  0   0 0 0 1\n' % lamb_0)
     wi.write('%d  %.6f  %.6f    %1.9e    %1.9e  %1.6e  %1.6e  %1.6e  %.6f\n'% (len(V), phase, phase, lmin, lmax, snV, ncont, dlamb, count))
     wi.write('\n')
     wmatrix(wi, V)
 
-def create_lsd_05(filename, phase_shift, vr_shift, vr_max, stokesV=False):
-    if stokesV:
-        files = glob.glob('lsd05_*')
-        nt = 2*len(files)
+def create_lsd(year, phase_shift, vr_shift, vr_max, iNorm=False):
+    if iNorm:
+        filename = 'v471tau_'+year+'.ss'
     else:
-        files = glob.glob('I_lsd05_*')
-        nt = len(files)
-    files.sort()
-    print(nt)
-    t = N.zeros(nt)
-    count = 0
-    for ift in files:
-        if stokesV:
-            with open('info'+ift[3:], 'r') as rt:
-                t[count] = float(rt.readline())
-                count += 1
-        else:
-            with open('I_info'+ift[5:], 'r') as rt:
-                t[count] = float(rt.readline())
-                count += 1
-    P.figure(figsize=(8,5))
-    count = 0
-    nexcluded = 0
+        filename = 'v471tau_'+year+'.s'
     with open(filename, 'w') as wi:
-        wi.write('LSD profiles of v471tau in 2005\n')
-        wi.write('%d\n' % (nt))
-        wi.write('\n')
-        for ifile in files:
-            data = N.genfromtxt(ifile, skip_header=2)
-            vr = data[:,0] - (vr_max*N.sin(2.*N.pi*((t[count] - To)/period + phase_shift)) + vr_shift)
-            I = data[:,1]; sI = data[:,2]
-            snI = I/sI
-            ics = N.abs(vr) < 110.
-            cvr = vr[ics]; cI = I[ics]
-            cycle = (t[count] - To)/period + phase_shift - 25151
-            count += 1
-            if stokesV:
+        PATH = '/Users/bzaire/lsd/spectra/v471tau/v471tau_int' + year
+        os.chdir(PATH)
+        files_int = N.genfromtxt('observed_data.txt', dtype=str)
+        nt_int = int(len(files_int))
+        print('Nint observations = ', nt_int)
+        files_int.sort()
+        t_int = N.zeros(nt_int)
+        P.figure(figsize=(8,5))
+        count = 0
+        nexcluded = 0
+        with open('observed_data_excluded.txt', 'w') as wud:
+            for ifile in files_int:
+                read_t = open(ifile, 'r')
+                t_int[count] = float(read_t.readline())
+                data = N.genfromtxt(ifile, skip_header=3)
+                cycle = (t_int[count] - To)/period + phase_shift 
+                vr = data[:,0] - (vr_max*N.sin(2.*N.pi*cycle) + vr_shift)
+                I = data[:,1]; sI = data[:,2]
+                snI = I/sI
+                ics = N.abs(vr) < 110.
+                cvr = vr[ics]; cI = I[ics]
+                cycle -= 14420. #subtract some cycles to make things more readable
+                count += 1
+                if(snI[0:25].mean()>=400.):
+                    P.plot(cvr, cI, 'k',linewidth=1) 
+                    P.text(-100., .95,s=r'$\phi$ = %1.4f, $v_\mathrm{rad}$ = %2.1f, k = %3.1f' %(phase_shift, vr_shift, vr_max), fontsize=10)
+                    P.plot(N.ones(10)*110.,N.linspace(0.98*cI.min(),1.02*cI.max(),10), '--k')
+                    P.plot(-N.ones(10)*110.,N.linspace(0.98*cI.min(),1.02*cI.max(),10), '--k')
+                    if iNorm:
+                        normI = norm(cvr, cI, cont=0.9993, fac=5.13) # norm data
+                    else:
+                        normI = cI
+                    # store result
+                    wfile(wi, cycle, cvr, snI.mean()*N.sqrt(1.), normI)
+                else:
+                    wud.write(ifile+'\n')
+                    nexcluded += 1
+        PATH = '/Users/bzaire/lsd/spectra/v471tau/v471tau_pol' + year
+        os.chdir(PATH)
+        files_pol = N.genfromtxt('observed_data.txt', dtype=str)
+        nt_pol = int(len(files_pol))
+        print('Npol observations = ', nt_pol)
+        files_pol.sort()
+        t_pol = N.zeros(nt_pol)
+        count = 0
+        with open('observed_data_excluded.txt', 'w') as wud:
+            for ifile in files_pol:
+                read_t = open(ifile, 'r')
+                t_pol[count] = float(read_t.readline())
+                data = N.genfromtxt(ifile, skip_header=3)
+                cycle = (t_pol[count] - To)/period + phase_shift 
+                vr = data[:,0] - (vr_max*N.sin(2.*N.pi*cycle) + vr_shift)
                 V = data[:,3]; sV = data[:,4]
                 snV = 1./sV
                 Null = data[:,5]; sN = data[:,6]
                 snN = 1./sN
                 cV = V[ics]; cNull = Null[ics]
-                if((snI[0:25].mean()>=500.) and (snV.mean()>=1000.)):
-                    P.plot(cvr, cI, 'k',linewidth=1) 
-                    P.text(-100., .95,s=r'$\phi$ = %1.4f, $v_\mathrm{rad}$ = %2.1f, k = %3.1f' %(phase_shift, vr_shift, vr_max), fontsize=10)
-                    P.plot(N.ones(10)*110.,N.linspace(0.98*cI.min(),1.02*cI.max(),10), '--k')
-                    P.plot(-N.ones(10)*110.,N.linspace(0.98*cI.min(),1.02*cI.max(),10), '--k')
+                cycle -= 14420. #subtract some cycles to make things more readable
+                if(snV.mean()>=1500.):
                     # store result
-                    #wfile_V(wi, cycle, cvr, snI.mean()*N.sqrt(1./0.35), cI, snV.mean(), cV)
-                    wfile_V(wi, cycle, cvr, snI.mean()*N.sqrt(1.), cI, snV.mean(), cV)
-                else:
-                    nexcluded += 2
-            else: 
-                if(snI[0:25].mean()>=300.):
-                    P.plot(cvr, cI, 'k',linewidth=1) 
-                    P.text(-100., .95,s=r'$\phi$ = %1.4f, $v_\mathrm{rad}$ = %2.1f, k = %3.1f' %(phase_shift, vr_shift, vr_max), fontsize=10)
-                    P.plot(N.ones(10)*110.,N.linspace(0.98*cI.min(),1.02*cI.max(),10), '--k')
-                    P.plot(-N.ones(10)*110.,N.linspace(0.98*cI.min(),1.02*cI.max(),10), '--k')
-                    # store result
-                    wfile(wi, cycle, cvr, snI.mean()*N.sqrt(1.), cI)
-                    #wfile(wi, cycle, cvr, snI.mean()*N.sqrt(1./0.35), cI)
+                    wfile_V(wi, cycle, cvr, snV.mean(), cV)
                 else:
                     nexcluded += 1
-    if nexcluded != 0:
-        print('Excluding %d nights in 2005' %nexcluded)
-        with open(filename, 'r') as file:
-            # read a list of lines into data
-            data = file.readlines()
-        # Changing second line
-        data[1] = '%d\n' %(nt - nexcluded)
-        # and write everything back
-        with open(filename, 'w') as file:
-            file.writelines(data)
-    print(nt-nexcluded)
-
-def create_lsd_04(filename, phase_shift, vr_shift, vr_max, stokesV=False):
-    if stokesV:
-        files = glob.glob('lsd04_*')
-        print(len(files))
-        nt = int(2.*len(files))
-        print(nt)
-    else:
-        files = glob.glob('I_lsd04_*')
-        nt = len(files)
-    files.sort()
-    print(nt)
-    t = N.zeros(nt)
-    count = 0
-    for ift in files:
-        if stokesV:
-            with open('info'+ift[3:], 'r') as rt:
-                t[count] = float(rt.readline())
-                count += 1
-        else:
-            with open('I_info'+ift[5:], 'r') as rt:
-                t[count] = float(rt.readline())
-                count += 1
-    P.figure(figsize=(8,5))
-    count = 0
-    nexcluded = 0
-    with open(filename, 'w') as wi:
-        wi.write('LSD profiles of v471tau in 2004\n')
-        wi.write('%d\n' % (nt))
-        wi.write('\n')
-        for ifile in files:
-            data = N.genfromtxt(ifile, skip_header=2)
-            vr = data[:,0] - (vr_max*N.sin(2.*N.pi*((t[count] - To)/period + phase_shift)) + vr_shift)
-            I = data[:,1]; sI = data[:,2]
-            snI = I/sI
-            ics = N.abs(vr) < 110.
-            cvr = vr[ics]; cI = I[ics]
-            cycle = (t[count] - To)/period + phase_shift - 24420
-            count += 1
-            if stokesV:
-                V = data[:,3]; sV = data[:,4]
-                snV = 1./sV
-                Null = data[:,5]; sN = data[:,6]
-                snN = 1./sN
-                cV = V[ics]; cNull = Null[ics]
-                if((snI[0:25].mean()>=500.) and (snV.mean()>=1000.)):
-                    P.plot(cvr, cI, 'k',linewidth=1) 
-                    P.text(-100., .95,s=r'$\phi$ = %1.4f, $v_\mathrm{rad}$ = %2.1f, k = %3.1f' %(phase_shift, vr_shift, vr_max), fontsize=10)
-                    P.plot(N.ones(10)*110.,N.linspace(0.98*cI.min(),1.02*cI.max(),10), '--k')
-                    P.plot(-N.ones(10)*110.,N.linspace(0.98*cI.min(),1.02*cI.max(),10), '--k')
-                    # store result
-#                    wfile_V(wi, cycle, cvr, snI.mean()*N.sqrt(1./0.35), cI, snV.mean(), cV)
-                    wfile_V(wi, cycle, cvr, snI.mean()*N.sqrt(1.), cI, snV.mean(), cV)
-                else:
-                    nexcluded += 2
-            else: 
-                if(snI[0:25].mean()>=0.009):
-                    P.plot(cvr, cI, 'k',linewidth=1) 
-                    P.text(-100., .95,s=r'$\phi$ = %1.4f, $v_\mathrm{rad}$ = %2.1f, k = %3.1f' %(phase_shift, vr_shift, vr_max), fontsize=10)
-                    P.plot(N.ones(10)*110.,N.linspace(0.98*cI.min(),1.02*cI.max(),10), '--k')
-                    P.plot(-N.ones(10)*110.,N.linspace(0.98*cI.min(),1.02*cI.max(),10), '--k')
-                    # store result
-                    #wfile(wi, cycle, cvr, snI.mean()*N.sqrt(1./0.35), cI)
-                    wfile(wi, cycle, cvr, snI.mean()*N.sqrt(1.), cI)
-                else:
-                    nexcluded += 1
-    if nexcluded != 0:
-        print('Excluding %d nights in 2004' %nexcluded)
-        with open(filename, 'r') as file:
-            # read a list of lines into data
-            data = file.readlines()
-        # Changing second line
-        data[1] = '%d\n' %(nt - nexcluded)
-        # and write everything back
-        with open(filename, 'w') as file:
-            file.writelines(data)
-    print(nt-nexcluded)
-
-    
-def norm_lsd_05(filename, fac=5.12, stokesV=False):
-    if stokesV:
-        phase, vr, snI, I, snV, V = rstokes(filename)
-        nObs = len(phase)
-        rnI = norm(vr, I, cont=0.9993, fac=fac)
-        with open(filename+"s", 'w') as wi:
-            wi.write('Spectrum of v471tau in 2005\n')
-            wi.write('%d\n' % (2*nObs))
-            wi.write('\n')
-            for i in range(nObs):
-                wfile_V(wi, phase[i], N.array(vr[i]), snI[i], N.array(rnI[i]), snV[i], N.array(V[i]))
-    else:
-        phase, vr, snI, I = rstokes(filename)
-        nObs = len(I)
-        rnI = norm(vr, I, cont=0.9993, fac=fac)
-        with open(filename+"s", 'w') as wi:
-            wi.write('Spectrum of v471tau in 2005\n')
-            wi.write('%d\n' % (nObs))
-            wi.write('\n')
-            for i in range(nObs):
-                wfile(wi, phase[i], N.array(vr[i]), snI[i], N.array(rnI[i]))
-
-def norm_lsd_04(filename, fac=5.12, stokesV=False):
-    if stokesV:
-        phase, vr, snI, I, snV, V = rstokes(filename)
-        nObs = len(phase)
-        rnI = norm(vr, I, cont=0.9993, fac=fac)
-        with open(filename+"s", 'w') as wi:
-            wi.write('Spectrum of v471tau in 2005\n')
-            wi.write('%d\n' % (2*nObs))
-            wi.write('\n')
-            for i in range(nObs):
-                wfile_V(wi, phase[i], N.array(vr[i]), snI[i], N.array(rnI[i]), snV[i], N.array(V[i]))
-    else:
-        phase, vr, snI, I = rstokes(filename)
-        nObs = len(phase)
-        rnI = norm(vr, I, cont=0.9993, fac=fac)
-        with open(filename+"s", 'w') as wi:
-            wi.write('Spectrum of v471tau in 2004\n')
-            wi.write('%d\n' % (nObs))
-            wi.write('\n')
-            for i in range(nObs):
-                wfile(wi, phase[i], N.array(vr[i]), snI[i], N.array(rnI[i]))
-
-
-def norm_lsd_search(filename, fit_cont, area):
-    phase, vr, snI, I = rstokes(filename)
-    nObs = len(I)
-    rnI = norm_search(vr, I, fit_cont, area, cont=0.9993, fac=5.12)
-    with open(filename+"s", 'w') as wi:
-        wi.write('Spectrum of v471tau in 2004\n')
-        wi.write('%d\n' % (nObs))
-        wi.write('\n')
-        for i in range(nObs):
-            wfile(wi, phase[i], N.array(vr[i]), snI[i], N.array(rnI[i]))
+    PATH = '/Users/bzaire/lsd/spectra/v471tau/'
+    os.chdir(PATH)
+    print('Excluding %d observations in 20%2s' %(nexcluded, year))
+    with open(filename, 'r') as file:
+        # read a list of lines into data
+        data = file.readlines()
+    # and write everything back
+    with open(filename, 'w') as file:
+        file.write('LSD profiles of v471tau in 20'+year+'\n')
+        file.write('%d\n' %(nt_int+nt_pol - nexcluded))
+        file.write('\n')
+        file.writelines(data)
+    print(nt_int+nt_pol-nexcluded)
--- zdi/norm.py
+++ zdi/norm.py
@@ -1,43 +1,11 @@
 import numpy as N
 import matplotlib.pyplot as P
 
-def norm(vr_matrix, matrix, cont=0.9992, fac = 5.1):
-    nObs = len(matrix)
-    rescale = []
-    for i in range(nObs):
-        stokes = N.array(matrix[i])
-        vr = N.array(vr_matrix[i])
-        ir = [0,1,2]; il = [-1,-2,-3]
-        vrr = vr[ir].mean(); vrl = vr[il].mean()
-        stokesr = stokes[ir].mean(); stokesl = stokes[il].mean()
-        fit_cont = N.poly1d(N.polyfit((vrr, vrl), (stokesr, stokesl), deg=1))
-        area = N.trapz(1- stokes/fit_cont(vr), dx=1.8)
-        rescale.append(cont - (1 - stokes/fit_cont(vr))*(fac/area))
-    return rescale
 
-def norm_search(vr_matrix, matrix, fit_cont, area, cont=0.9992, fac = 5.1):
-    # input:
-    #        fit_cont : list with function containing the 1th degree polynomial for each observation.
-    #        area : list with the areas computed for each observation.
-    nObs = len(matrix)
-    rescale = []
-    for i in range(nObs):
-        stokes = N.array(matrix[i])
-        vr = N.array(vr_matrix[i])
-        ir = [0,1,2]; il = [-1,-2,-3]
-        rescale.append(cont - (1 - stokes/fit_cont[i](vr))*(fac/area[i]))
-    return rescale
-
-def find_cont(vr_matrix, matrix, cont=0.9992, fac = 5.1):
-    nObs = len(matrix)
-    area = N.zeros(nObs)
-    rescale = []
-    for i in range(nObs):
-        stokes = N.array(matrix[i])
-        vr = N.array(vr_matrix[i])
-        ir = [0,1,2]; il = [-1,-2,-3]
-        vrr = vr[ir].mean(); vrl = vr[il].mean()
-        stokesr = stokes[ir].mean(); stokesl = stokes[il].mean()
-        area[i] = N.trapz(1 - stokes/fit_cont[i](vr), dx=1.8)
-        rescale.append(cont - (1 - stokes/fit_cont[i](vr))*(fac/area[i]))
-    return area
+def norm(vr, I, cont=0.9992, fac = 5.1):
+    ir = [0,1,2]; il = [-1,-2,-3]
+    vrr = vr[ir].mean(); vrl = vr[il].mean()
+    stokesr = I[ir].mean(); stokesl = I[il].mean()
+    fit_cont = N.poly1d(N.polyfit((vrr, vrl), (stokesr, stokesl), deg=1))
+    area = N.trapz(1 - I/fit_cont(vr), dx=1.8)
+    return cont - (1 - I/fit_cont(vr))*(fac/area)
